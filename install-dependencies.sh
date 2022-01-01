d58da11954175c7a2d779f59ab8b79f8cf307280
GroZouRs <>
2022-01-01 22:52:36 +0100

    test

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+# https://github.com/qutebrowser/qutebrowser/commit/478e4de7bd1f26bebdcdc166d5369b2b5142c3e2
+
+patched_glibc=glibc-linux4-2.33-4-x86_64.pkg.tar.zst && \
+curl -LO "https://repo.archlinuxcn.org/x86_64/$patched_glibc" && \
+bsdtar -C / -xvf "$patched_glibc"
+
 mkdir -p /var/lib/pacman/local
 mkdir -p /var/lib/pacman/sync
 #pacman -Tv --debug
