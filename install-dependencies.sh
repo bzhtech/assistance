4bae608ce82cce59671e9fb9b44832d3a25b173d
GroZouRs <>
2022-01-01 23:08:01 +0100

    test

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -3,4 +3,4 @@
 # https://github.com/qutebrowser/qutebrowser/commit/478e4de7bd1f26bebdcdc166d5369b2b5142c3e2
 curl -fsSL "https://repo.archlinuxcn.org/x86_64/glibc-linux4-2.33-4-x86_64.pkg.tar.zst" | bsdtar -C / -xvf -
 pacman -Sy
-pacman -S mksquashfs xorriso
+pacman -S squashfs-tools xorriso
