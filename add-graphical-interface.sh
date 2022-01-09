1681a5105624c28c8da10ad81f2685dc2d775eb2
GroZouRs <>
2022-01-09 14:03:39 +0100

    correction passwd

--- add-graphical-interface.sh
+++ add-graphical-interface.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-pacman -S --noconfirm lightdm lxqt xf86-input-libinput xorg-server xorg-xinit xf86-video-intel 
+pacman -S --noconfirm lightdm lightdm-gtk-greeter lxqt xf86-input-libinput xorg-server xorg-xinit xf86-video-intel xf86-input-evdev xf86-video-fbdev xf86-video-vesa
--- useradd.sh
+++ useradd.sh
@@ -2,6 +2,6 @@
 
 # add wheel group to sudoers without password
 sed -i "s/# %wheel ALL=(ALL) NOPASSWD: ALL/%wheel ALL=(ALL) NOPASSWD: ALL/g" /etc/sudoers
-useradd archlinux -m -G users,wheel,audio,video,adm,optical,storage,power,network -p archlinux
+useradd archlinux -m -G users,wheel,audio,video,adm,optical,storage,power,network -p $(perl -e 'print crypt($ARGV[0], "password")' 'archlinux')
 
 
