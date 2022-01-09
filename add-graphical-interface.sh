1416fc8e6f3802fee14210c31ba52a5b9b12f85b
GroZouRs <>
2022-01-09 23:21:05 +0100

    nmapplet + firefox + lightdm start&autologin

--- add-graphical-interface.sh
+++ add-graphical-interface.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-pacman -S --noconfirm lightdm lightdm-gtk-greeter lxqt xf86-input-libinput xorg-server xorg-xinput xorg-xinit xf86-video-intel xf86-input-evdev xf86-video-fbdev xf86-video-vesa
+pacman -S --noconfirm lightdm lightdm-gtk-greeter lxqt xf86-input-libinput xorg-server xorg-xinput xorg-xinit xf86-video-intel xf86-input-evdev xf86-video-fbdev xf86-video-vesa network-manager-applet firefox
+systemctl enable lightdm
+
+# apply autologin on lightdm
+sed -i "s/#pam-service=/pam-service=/g" /etc/lightdm/lightdm.conf
+sed -i "s/#pam-autologin-service=/pam-autologin-service=/g" /etc/lightdm/lightdm.conf
+sed -i "s/#autologin-user-timeout=/autologin-user-timeout=/g" /etc/lightdm/lightdm.conf
+echo -e "autologin-user=archlinux" >> /etc/lightdm/lightdm.conf
--- useradd.sh
+++ useradd.sh
@@ -2,6 +2,5 @@
 
 # add wheel group to sudoers without password
 sed -i "s/# %wheel ALL=(ALL) NOPASSWD: ALL/%wheel ALL=(ALL) NOPASSWD: ALL/g" /etc/sudoers
-useradd archlinux -m -G users,wheel,audio,video,adm,optical,storage,power,network -p $(perl -e 'print crypt($ARGV[0], "password")' 'archlinux')
-
-
+groupadd -r autologin
+useradd archlinux -m -G users,wheel,audio,video,adm,optical,storage,power,network,autologin -p $(perl -e 'print crypt($ARGV[0], "password")' 'archlinux')
