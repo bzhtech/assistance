61b1875f5f3c0e4cfaa729d41a3537ea2c9267a0
bzhtech <96878335+bzhtech@users.noreply.github.com>
2022-01-10 13:13:58 +0100

    Update add-graphical-interface.sh

--- add-graphical-interface.sh
+++ add-graphical-interface.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 pacman -S --noconfirm lightdm lightdm-gtk-greeter lxqt xf86-input-libinput xorg-server xorg-xinput xorg-xinit xf86-video-intel xf86-input-evdev xf86-video-fbdev xf86-video-vesa network-manager-applet firefox
 systemctl enable lightdm
+
+# switch from iwd to NetworkManager 
 systemctl disable iwd
+systemctl enable NetworkManager
 
 # apply autologin on lightdm
 sed -i "s/#pam-service=/pam-service=/g" /etc/lightdm/lightdm.conf
