f23fa3c7cc6446260a03ffc2f0162fb83cbbfa0c
bzhtech <96878335+bzhtech@users.noreply.github.com>
2022-01-21 02:09:34 +0100

    correction gpg repopulate post archlinux-keyring upgrade

--- update-pacman-database.sh
+++ update-pacman-database.sh
@@ -10,9 +10,11 @@ sed -i "s/CheckSpace/#CheckSpace/g" /etc/pacman.conf
 pacman-key --init
 pacman-key --populate archlinux
 
-
 # update pacman database
 pacman -Sy
+
+# repopulate pacman gpg keys
+pacman-key --populate archlinux
 
 #pid_gpg_to_kill=$(ps -eaf | grep [gpg]-agent | awk '{print $2}')
 #kill -9 $pid_gpg_to_kill
